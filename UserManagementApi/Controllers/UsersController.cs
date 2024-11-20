c5768e186dcf247533753f60fa07004d2b74cacd
Emircan EGE <ege61emir@gmail.com>
2024-11-20 23:55:44 +0300

    Add user controller with CRUD endpoints.

--- UserManagementApi/Controllers/UsersController.cs
+++ UserManagementApi/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserManagementApi.Core.Model;
 using UserManagementApi.Data;
 
 namespace UserManagementApi.Controllers
@@ -14,5 +16,59 @@ namespace UserManagementApi.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] User UpdatedUser)
+        {
+            if (id != UpdatedUser.Id) return BadRequest("User id mismatch");
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+
+            user.FirstName = UpdatedUser.FirstName;
+            user.LastName = UpdatedUser.LastName;
+            user.Email = UpdatedUser.Email;
+            user.DateOfBirth = UpdatedUser.DateOfBirth;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        
     }
 }
