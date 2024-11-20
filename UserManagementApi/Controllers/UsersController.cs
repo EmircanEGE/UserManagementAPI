b79b7b29b758fb7eaec246b5c1a2e95b1130ab8d
Emircan EGE <ege61emir@gmail.com>
2024-11-20 19:52:50 +0300

    Initialize database context and configure SQLite connection.

---
+++ UserManagementApi/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserManagementApi.Data;
+
+namespace UserManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public UsersController(Context context)
+        {
+            _context = context;
+        }
+    }
+}
