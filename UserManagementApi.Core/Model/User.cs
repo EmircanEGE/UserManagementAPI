a934f5e669813ecee83e3972f20ec5c5b4d3f63a
Emircan EGE <ege61emir@gmail.com>
2024-11-21 10:39:26 +0300

    Add validation for user model.

--- UserManagementApi.Core/Model/User.cs
+++ UserManagementApi.Core/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,18 @@ namespace UserManagementApi.Core.Model
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DateOfBirth { get; set; }
     }
 }
